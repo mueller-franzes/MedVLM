50577e9f3e3537d753bfe2aad10fe9a9ef681c82
mueller-franzes <gustav.mueller-franzes@rwth-aachen.de>
2025-04-01 14:21:49 +0000

    cleanup

--- tests/models/test_medvlm.py
+++ tests/models/test_medvlm.py
@@ -7,14 +7,14 @@ from medvlm.models.medvlm import MedVLM
 input = torch.randn((1, 3, 32, 224, 224))
 
 # ---- Random text ---------
-tokenizer = Tokenizer(model_name="meta-llama/Llama-3.2-1B") # model_name="meta-llama/Llama-3.2-1B"
+tokenizer = Tokenizer() # model_name="meta-llama/Llama-3.2-1B"
 text = "Das ist ein Test"
 text_tokens = tokenizer(text)[None] # [B, 7]
 
 
 # ---- Model ---------
 model = MedVLM(tokenizer_y=tokenizer)
-device=torch.device('cuda')
+device=torch.device('cuda:8')
 model.to(device)
 
 # ---- Number of parameters ---------
--- tests/models/test_medvlm_step.py
+++ tests/models/test_medvlm_step.py
@@ -10,21 +10,21 @@ from medvlm.data.datamodule import DataModule
 from medvlm.models.tokenizer import Tokenizer
 
 
-tokenizer = Tokenizer(model_name="google/gemma-3-1b-pt")
+tokenizer = Tokenizer(model_name="meta-llama/Llama-3.2-1B") # model_name="meta-llama/Llama-3.2-1B"
 
 # ds_test = CTRATE_Dataset3D(split='test', tokenizer=tokenizer)
 ds_test = UKA_Dataset3D(split='test', tokenizer=tokenizer)
 # ds_test.LABEL = "Pleural effusion"
 ds_test.LABEL = "Karzinom"
 
-device=torch.device('cuda')
+device=torch.device('cuda:8')
 
-model = MedVLM(tokenizer_y=tokenizer)
+model = MedVLM(tokenizer_y=tokenizer, use_llm=True)
 # model = MedVLM.load_from_checkpoint('runs/UKA/MedVLM_2025_02_23_215831/epoch=12-step=5902.ckpt')
 model.to(device)
-model.save_attn = False
 
-dm = DataModule(ds_test=ds_test, batch_size=2, num_workers=0, num_train_samples=10)
+
+dm = DataModule(ds_test=ds_test, batch_size=1, num_workers=0, num_train_samples=10)
 dl = dm.test_dataloader() 
 for idx, batch in tqdm(enumerate(iter(dl))):
     batch = {k: v.to(device) if isinstance(v, torch.Tensor) else v for k, v in batch.items()}
